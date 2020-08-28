502b5949fa9de52184e0b2196cd822b4a40c997d
yanis <yanis.hammada@ricardo.ch>
2020-08-28 21:11:57 +0200

    Longest substring without duplicates

---
+++ 026-longest-substring-without-dupes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLongestSubstring(self, input_string: str) -> int:
+        if not input_string:
+            return 0
+
+        max_len,  head_cursor, tail_cursor, sub_str_chars = 0, 0, 0, set()
+
+        while tail_cursor < len(input_string) and head_cursor < len(input_string):
+            current_char = input_string[head_cursor]
+
+            if current_char not in sub_str_chars:
+                sub_str_chars.add(current_char)
+                head_cursor += 1
+                max_len = max(max_len, head_cursor - tail_cursor)
+            else:
+                sub_str_chars.remove(input_string[tail_cursor])
+                tail_cursor += 1
+
+        return max_len
+
+
+print(Solution().lengthOfLongestSubstring('abcabcbb'))
+print(Solution().lengthOfLongestSubstring('bbbbb'))
+print(Solution().lengthOfLongestSubstring('pwwkew'))
+print(Solution().lengthOfLongestSubstring('aab'))
+print(Solution().lengthOfLongestSubstring('dvdf'))
