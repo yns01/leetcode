542ae73285614fc0aa0818cadf92a7c348613ac5
yanis <yanis.hammada@ricardo.ch>
2020-08-29 19:05:53 +0200

    strstr return 0 if needle is empty

--- 008-strstr.py
+++ 008-strstr.py
@@ -1,7 +1,7 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if not needle:
-            return -1
+            return 0
 
         for i in range(len(haystack) - len(needle) + 1):
             for j in range(len(needle)):
