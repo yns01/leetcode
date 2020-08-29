b4ce44145767c869fc43c99d3c9710b8b01d3776
yanis <yanis.hammada@ricardo.ch>
2020-08-29 19:04:15 +0200

    O(n*m) strstr

--- 008-strstr.py
+++ 008-strstr.py
@@ -1,54 +1,24 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if not needle:
-            return 0
+            return -1
 
-        haystack_cursor, needle_cursor, matching_position_start = 0, 0, -1
+        for i in range(len(haystack) - len(needle) + 1):
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
 
-        while haystack_cursor < len(haystack):
-            if needle_cursor == len(needle):
-                return matching_position_start
-
-            current_haystack_char = haystack[haystack_cursor]
-            current_needle_char = needle[needle_cursor]
-
-            if current_haystack_char == current_needle_char:
-                if matching_position_start == -1:
-                    matching_position_start = haystack_cursor
-
-                needle_cursor += 1
-                haystack_cursor += 1
-            else:
-                needle_cursor = 0
-
-                if matching_position_start != -1:
-                    haystack_cursor = matching_position_start + 1
-                else:
-                    haystack_cursor += 1
-
-                matching_position_start = -1
-
-        if needle_cursor == len(needle):
-            return matching_position_start
-
-        return -1
-
-    def strStr2(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-
-        for i in range(0, len(haystack)-len(needle) + 1):
-            if haystack[i:i+len(needle)] == needle:
-                return i
+                if j == len(needle) - 1:
+                    return i
 
         return -1
 
 
 s = Solution()
-print(s.strStr2("hello", 'll'))
-print(s.strStr2("helllok", 'lok'))
-print(s.strStr2("helllokl", 'lok'))
-print(s.strStr2("lolok", 'lok'))
-print(s.strStr2("lolo", 'lok'))
-print(s.strStr2("lo", 'lok'))
-print(s.strStr2("mississippi", 'issip'))
+print(s.strStr("hello", 'll'))
+print(s.strStr("helllok", 'lok'))
+print(s.strStr("helllokl", 'lok'))
+print(s.strStr("lolok", 'lok'))
+print(s.strStr("lolo", 'lok'))
+print(s.strStr("lo", 'lok'))
+print(s.strStr("mississippi", 'issip'))
