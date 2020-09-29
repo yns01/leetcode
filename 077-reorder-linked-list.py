56f7431886065a38e53cd1b224af3b6cb9e1ef82
yanis <yanis.hammada@ricardo.ch>
2020-09-29 19:17:16 +0200

    Re-order linked list v2

--- 077-reorder-linked-list.py
+++ 077-reorder-linked-list.py
@@ -35,6 +35,42 @@ class Solution:
         current.next = None
         return head
 
+    def reorderListv2(self, head: ListNode) -> None:
+        if not head or not head.next:
+            return
+
+        slow, fast = head, head.next
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        current = slow
+        prev = None
+        while current:
+            nxt = current.next
+            current.next = prev
+            prev = current
+
+            current = nxt
+
+        # merge two list
+        current_reversed = prev
+        current = head
+
+        while current_reversed:
+            if not current:
+                break
+
+            nxt = current.next
+            nxt2 = current_reversed.next
+            current.next = current_reversed
+            current_reversed.next = nxt
+
+            current_reversed = nxt2
+            current = nxt
+
+        return head
+
     def print_list(self, head: ListNode):
         if not head:
             print('empty list')
@@ -50,4 +86,6 @@ l3 = ListNode(3, l4)
 l2 = ListNode(2, l3)
 l1 = ListNode(1, l2)
 
-Solution().print_list(Solution().reorderList(l1))
+# Solution().print_list(Solution().reorderList(l1))
+print("next")
+Solution().print_list(Solution().reorderListv2(l1))
