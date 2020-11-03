a8726535be396baf9f6c9c1165508f6905d3cc17
yanis <yanis.hammada@ricardo.ch>
2020-11-03 11:24:26 +0100

    Partition version of find Kth

--- 101-kth-largest-element-in-array.py
+++ 101-kth-largest-element-in-array.py
@@ -1,9 +1,10 @@
 from typing import List
 import heapq
+import random
 
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargestv1(self, nums: List[int], k: int) -> int:
         if not nums:
             return 0
 
@@ -16,6 +17,41 @@ class Solution:
 
         return h[0]
 
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return -1
 
-print(Solution().findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
+        left, right, kth = 0, len(nums) - 1, None
+
+        while True:
+            kth = self.partition(nums, left, right)
+            if kth == (len(nums) - k):
+                return nums[kth]
+            elif kth > (len(nums) - k):
+                right = kth - 1
+            else:
+                left = kth + 1
+
+        return -1
+
+    def partition(self, nums: List[int], left: int, right: int) -> int:
+        rand = random.randint(left, right)
+        nums[rand], nums[right] = nums[right], nums[rand]
+
+        pivot = nums[right]
+
+        frontier = left
+        while left < right:
+            if nums[left] < pivot:
+                nums[frontier], nums[left] = nums[left], nums[frontier]
+                frontier += 1
+
+            left += 1
+
+        nums[right], nums[frontier] = nums[frontier], nums[right]
+
+        return frontier
+
+
+print(Solution().findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 1))
 print(Solution().findKthLargest([3, 2, 1, 5, 6, 4], 2))
--- 105-k-closest-to-origin.py
+++ 105-k-closest-to-origin.py
@@ -1,9 +1,10 @@
 import heapq
 from typing import List
+import random
 
 
 class Solution:
-    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+    def kClosestv1(self, points: List[List[int]], K: int) -> List[List[int]]:
         if not K or not points:
             return []
 
@@ -24,6 +25,44 @@ class Solution:
             k_closest.append([x, y])
 
         return k_closest
+
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        if not K or not points:
+            return []
+
+        left, right = 0, len(points) - 1
+        while True:
+            kth = self.partition(points, left, right)
+            if kth == K - 1:
+                return points[:K]
+            elif kth > K - 1:
+                right = kth - 1
+            else:
+                left = kth + 1
+
+        return - 1
+
+    def get_distance_to_origin(self, p):
+        x, y = p
+        return x**2 + y**2
+
+    def partition(self, points, left, right):
+        rand = random.randint(left, right)
+        points[right], points[rand] = points[rand], points[right]
+
+        frontier = left
+        pivot = points[right]
+
+        while left < right:
+            if self.get_distance_to_origin(points[left]) < self.get_distance_to_origin(pivot):
+                points[frontier], points[left] = points[left], points[frontier]
+                frontier += 1
+
+            left += 1
+
+        points[frontier], points[right] = points[right], points[frontier]
+
+        return frontier
 
 
 print(Solution().kClosest([[3, 3], [5, -1], [-2, 4]], 2))
