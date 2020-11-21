635b0401f8abe2a7055dc9244016dcf22d1da568
yanis <yanis.hammada@ricardo.ch>
2020-11-21 15:12:45 +0100

    Alternative approach for Kth most frequent words

--- 134-top-k-frequent-words.py
+++ 134-top-k-frequent-words.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import Counter
+import heapq
 
 
 class Solution:
@@ -12,15 +13,32 @@ class Solution:
 
         result = []
 
-        index, elements = len(freq_to_elements) - 1, 0
-        while elements < k:
+        index = len(freq_to_elements) - 1
+        while len(result) < k:
             result.extend(sorted(freq_to_elements[index]))
-            elements += len(freq_to_elements[index])
-
             index -= 1
 
         return result[:k]
 
+    def topKFrequentV1(self, words: List[str], k: int) -> List[str]:
+        frequencies = Counter(words)
+
+        heap = []
+        for word, freq in frequencies.items():
+            heap.append((-freq, word))
+
+        heapq.heapify(heap)
+
+        result = []
+        while len(result) < k:
+            result.append(heapq.heappop(heap)[1])
+
+        return result
+
 
 print(Solution().topKFrequent(
     ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], 4))
+
+
+print(Solution().topKFrequentV1(
+    ["i", "love", "leetcode", "i", "love", "coding"], 2))
