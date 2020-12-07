a63ea9b559459fe36bacbe5cacab9f494eb476a4
yanis <yanis.hammada@ricardo.ch>
2020-12-07 22:12:47 +0100

    Simplify word search

--- 149-word-search.py
+++ 149-word-search.py
@@ -12,15 +12,25 @@ class Solution:
         return False
 
     def dfs(self, board, starting_point, word, word_index, seen):
-        if word_index >= len(word):
-            return True
-
         r, c = starting_point
         if board[r][c] != word[word_index]:
             return False
 
         seen.add(starting_point)
         word_index += 1
+
+        # In the following example,
+        # [["a","b"],
+        # ["c","d"]]
+        # "acdb"
+        # When we visit the last node, 'b', we don't have anything else to visit as
+        # either we already saw the nodes, or they are out of range.
+        # If we already found the string, we just stop.
+        # An alternative would be to check for valid boundaries at the beginning of the function,
+        # right after the word_index check.
+
+        if word_index >= len(word):
+            return True
 
         for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
             nr = r + dy
@@ -34,17 +44,7 @@ class Solution:
         # we remove the point from seen to make it available for future explorations.
         seen.remove(starting_point)
 
-        # In the following example,
-        # [["a","b"],
-        # ["c","d"]]
-        # "acdb"
-        # When we visit the last node, 'b', we don't have anything else to visit as
-        # either we already saw the nodes, or they are out of range.
-        # If we already found the string, we just stop.
-        # An alternative would be to check for valid boundaries at the begininng of the function,
-        # right after the word_index check.
-
-        return True if word_index == len(word) else False
+        return False
 
 
 print(Solution().exist([["A", "B", "C", "E"], [
