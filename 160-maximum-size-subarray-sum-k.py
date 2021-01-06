6b8a2acd5370d6e14f3d453c26176dc6011a0237
yanis <yanis.hammada@ricardo.ch>
2021-01-06 23:02:56 +0100

    Simplify max subarray sums equals k

--- 160-maximum-size-subarray-sum-k.py
+++ 160-maximum-size-subarray-sum-k.py
@@ -6,14 +6,22 @@ class Solution:
         if not nums:
             return 0
 
-        running_sum, sums = 0, {0: -1}
+        running_sum, sums = 0, {}
         max_len = 0
 
         for i, n in enumerate(nums):
             running_sum += n
 
             complement = running_sum - k
-            if complement in sums:
+
+            # If the complement is 0, it means that the running sum from
+            # the start of the array to i equals k. In that case, the len of
+            # the sub array is i+1
+            if complement == 0:
+                max_len = max(max_len, i+1)
+            # Otherwise, the len of the subarray is i - sums[complement]
+            # which is the index of the complement.
+            elif complement in sums:
                 max_len = max(max_len, i - sums[complement])
 
             if running_sum not in sums:
