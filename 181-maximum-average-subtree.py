b770819ea91e7cfa5a22fb4aca30ff8abc54c8c1
yanis <yanis.hammada@ricardo.ch>
2021-01-20 23:21:42 +0100

    Add variation for average subtree

--- 181-maximum-average-subtree.py
+++ 181-maximum-average-subtree.py
@@ -29,6 +29,44 @@ class Solution:
         recurse(root)
         return self.res
 
+    # Variation: return true if a node's value is the average of all its children
+    def average_equals(self, root: TreeNode) -> bool:
+        def recurse(node):
+            if not node:
+                return (False, 0, 0)
+
+            found, ls, lc = recurse(node.left)
+            if found:
+                return (True, 0, 0)
+
+            found, rs, rc = recurse(node.right)
+            if found:
+                return (True, 0, 0)
+
+            # We consider that a single node without children does not equals the average of its
+            # non existent children. Otherwise, a root node with value 0, without children
+            # would return true.
+
+            children_sum, children_count, children_avg = ls + rs, lc + rc, None
+            if children_count != 0:
+                children_avg = children_sum / children_count
+
+            if children_avg is not None and node.val == children_avg:
+                return (True, 0, 0)
+
+            current_sum = ls + rs + node.val
+            current_count = lc + rc + 1
+
+            return (False, current_sum, current_count)
+
+        if not root:
+            return False
+
+        return recurse(root)[0]
+
 
 root = TreeNode(5, TreeNode(6), TreeNode(1))
 print(Solution().maximumAverageSubtree(root))
+
+root = TreeNode(3, TreeNode(1, TreeNode(4)), TreeNode(4))
+print(Solution().average_equals(root))
