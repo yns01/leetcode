293b1ca6812a86ba374a0035fcc3a0b1eb5ddaec
yanis <yanis.hammada@ricardo.ch>
2021-02-03 20:05:21 +0100

    Add simpler version for LCA

--- 080-lowest-common-ancestor-tree.py
+++ 080-lowest-common-ancestor-tree.py
@@ -19,6 +19,21 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        if root is p or root is q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left or right
+
+    def lowestCommonAncestorv1(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         if not root or not p or not q:
             return None
 
